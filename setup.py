5d29bac5bfba367608abe922372f545e234b85c1
Koen Vervloesem <koen@vervloesem.eu>
2022-12-20 12:09:35 +0100

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -33,5 +33,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
